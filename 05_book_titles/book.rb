2193d1ad815d0553cbf8c1d83408936edbc3429d
Mohamed Zouari <mensae@posteo.org>
2021-04-22 18:34:37 +0100

    Solve 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
-# write your code here
+  attr_writer :title
+
+  def initialize
+    @title = ""
+  end
+
+  def title
+    output_string = ""
+    little_words = ["and", "the", "in", "of", "an", "a"]
+
+    @title.split.each_with_index do |word, index|
+      output_string << if index.zero? # capitalize the first word
+        "#{word.capitalize} "
+      elsif little_words.include? word # do not capitalize "little words"
+        "#{word} "
+      else
+        "#{word.capitalize} " # capitalize words that are NOT "little words" including i
+      end
+    end
+    output_string.strip
+  end
 end
