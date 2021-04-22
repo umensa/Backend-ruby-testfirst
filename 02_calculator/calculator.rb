271aed4907408d85b6bab4ca1ec1171d462e9edd
Mohamed Zouari <mensae@posteo.org>
2021-04-22 18:12:57 +0100

    Write tests and code for Extra Credit Test-Driving Bonus

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -18,10 +18,28 @@ def sum(array)
 end
 
 # multiply which multiplies several numbers of an array together
-def multiply(array)
+def multiply(*array)
   total = 1
   array.each do |item|
     total *= item
   end
   total
 end
+
+# power which raises one number to the power of another number
+def power(num_one, num_two)
+  num_one**num_two
+end
+
+# [factorial](http://en.wikipedia.org/wiki/Factorial) (check Wikipedia if you forgot your high school math).
+def factorial(num)
+  result = 1
+  num.times do |number|
+    puts "Number: #{number}"
+    result *= (number + 1)
+    puts "Result: #{result}"
+  end
+  result
+end
+
+puts factorial(5)
